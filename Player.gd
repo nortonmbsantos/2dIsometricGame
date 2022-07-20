1bfd842cc457b4e503c4ead74291b936fd007b18
Norton <norton@solutionsource.com.br>
2022-07-20 15:46:15 -0300

    Adding folder for Scenes and adding animation to player movement

--- Player.gd
+++ Player.gd
@@ -3,19 +3,23 @@ extends KinematicBody2D
 var speed = 200  # speed in pixels/sec
 var velocity = Vector2.ZERO
 
-func get_input():
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed('move_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('move_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('move_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('move_up'):
-		velocity.y -= 1
-	# Make sure diagonal movement isn't faster
-	velocity = velocity.normalized() * speed
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
-	get_input()
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Move/blend_position", input_vector)
+		animationState.travel("Move")
+		velocity = velocity.move_toward(input_vector * 100, 100)
+	else:
+		animationState.travel("Idle")
+		velocity = Vector2.move_toward(Vector2.ZERO, 0)
+	
 	velocity = move_and_slide(velocity)
